d960f2105e9b11ef1acfd9ec79ee5cbb818e11ea
Avery Yen <ayen@pivotal.io>
2017-09-11 17:53:34 -0400

    Remove unneeded initializer

--- src/test/kotlin/RecentlyUsedListTest.kt
+++ src/test/kotlin/RecentlyUsedListTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 
 @DisplayName("RecentlyUsedList")
 class RecentlyUsedListTest {
-    private var list = RecentlyUsedList()
+    private lateinit var list: RecentlyUsedList
 
     @BeforeEach
     fun setUp() {
