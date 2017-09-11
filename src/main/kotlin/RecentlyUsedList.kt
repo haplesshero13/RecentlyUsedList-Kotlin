59ead6212b8708322f0bc9a8b38b488d97f846f6
Avery Yen <ayen@pivotal.io>
2017-09-11 13:59:44 -0400

    Sample solution.

--- src/main/kotlin/RecentlyUsedList.kt
+++ src/main/kotlin/RecentlyUsedList.kt
@@ -1,1 +1,18 @@
-class RecentlyUsedList
+class RecentlyUsedList {
+    private var elements = mutableListOf<String>()
+
+    val size: Int
+        get() = elements.size
+
+    operator fun get(i: Int): String {
+        return elements[i]
+    }
+
+    fun insert(element: String) {
+        val index = elements.indexOf(element)
+        if (index > -1) {
+            elements.removeAt(index)
+        }
+        elements.add(0, element)
+    }
+}
--- src/test/kotlin/RecentlyUsedListTest.kt
+++ src/test/kotlin/RecentlyUsedListTest.kt
@@ -1,12 +1,45 @@
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 @DisplayName("RecentlyUsedList")
 class RecentlyUsedListTest {
+    private var list = RecentlyUsedList()
+
+    @BeforeEach
+    fun setUp() {
+        list = RecentlyUsedList()
+    }
+
     @Test
-    @DisplayName("can run tests")
-    fun testList() {
-        assertEquals("It's a test!", "But it's the wrong test.")
+    @DisplayName("starts empty")
+    fun testListStartsEmpty() {
+        assertEquals(list.size, 0)
+    }
+
+    @Test
+    @DisplayName("keeps the order of inserted elements")
+    fun testInsert() {
+        list.insert("first element")
+        list.insert("second element")
+
+        assertEquals(list.size, 2)
+        assertEquals(list[0], "second element")
+        assertEquals(list[1], "first element")
+    }
+
+    @Test
+    @DisplayName("moves re-inserted elements to the front of the list")
+    fun testInsertExistingElement() {
+        list.insert("first element")
+        list.insert("second element")
+        list.insert("third element")
+        list.insert("first element")
+
+        assertEquals(list[0], "first element")
+        assertEquals(list[1], "third element")
+        assertEquals(list[2], "second element")
+        assertEquals(list.size, 3)
     }
 }
