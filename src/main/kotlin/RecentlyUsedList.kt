f6f769ce52cb4c8f2604f0484454a64b0b25e2ea
Avery Yen <haplesshero13@gmail.com>
2019-08-12 10:57:07 -0400

    Small refactor

--- src/main/kotlin/RecentlyUsedList.kt
+++ src/main/kotlin/RecentlyUsedList.kt
@@ -1,18 +1,15 @@
 class RecentlyUsedList {
-    private var elements = mutableListOf<String>()
+    private var elements =
+        mutableListOf<String>()
 
     val size: Int
         get() = elements.size
 
-    operator fun get(i: Int): String {
-        return elements[i]
-    }
+    operator fun get(i: Int): String =
+        elements[i]
 
     fun insert(element: String) {
-        val index = elements.indexOf(element)
-        if (index > -1) {
-            elements.removeAt(index)
-        }
+        elements.remove(element)
         elements.add(0, element)
     }
 }
